cf2adb0cee2f39dc40af033f70ee90bb30839a2f
Sulaimon Ekundayo <sulaimon.ekundayo@tutors.es>
2025-04-13 15:35:14 +0300

    Update middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,21 +1,28 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-      {
-        name: 'strapi::security',
-        config: {
-          contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-              'connect-src': ["'self'", 'https:'],
-              'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-              'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-              upgradeInsecureRequests: null,
-            },
-          },
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'https://portfolio-strapi-tv57.onrender.com'], 
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
         },
       },
-  'strapi::cors',
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173', 'https://ekundayosulaimon.netlify.app/'], 
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
