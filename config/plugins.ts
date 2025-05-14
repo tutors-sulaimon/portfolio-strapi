70f5e562e97307864420b98e381068eb23597071
Sulaimon Ekundayo <sulaimon.ekundayo@tutors.es>
2025-05-14 21:55:41 +0300

    Update strapi plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,48 @@
+// export default ({ env }) => ({
+//   // ...
+//   upload: {
+//     config: {
+//       provider: 'cloudinary',
+//       providerOptions: {
+//         cloud_name: env('CLOUDINARY_NAME'),
+//         api_key: env('CLOUDINARY_KEY'),
+//         api_secret: env('CLOUDINARY_SECRET'),
+//       },
+//       actionOptions: {
+//         upload: {},
+//         uploadStream: {},
+//         delete: {},
+//       },
+//     },
+//   },
+//   // ...
+// });
+
+// module.exports = ({ env }) => ({
+//   // ...
+//   email: {
+//     config: {
+//       provider: 'nodemailer',
+//       providerOptions: {
+//         host: env('SMTP_HOST'),
+//         port: env('SMTP_PORT'),
+//         auth: {
+//           user: env('SMTP_USERNAME'),
+//           pass: env('SMTP_PASSWORD'),
+//         },
+//         // ... any custom nodemailer options
+//       },
+//       settings: {
+//         defaultFrom: 'duniola4u@gmail.com',
+//         defaultReplyTo: 'duniola4u@gmail.com',
+//       },
+//     },
+//   },
+//   // ...
+// });
+
+
 export default ({ env }) => ({
-  // ...
   upload: {
     config: {
       provider: 'cloudinary',
@@ -15,22 +58,17 @@ export default ({ env }) => ({
       },
     },
   },
-  // ...
-});
-
-module.exports = ({ env }) => ({
-  // ...
   email: {
     config: {
       provider: 'nodemailer',
       providerOptions: {
         host: env('SMTP_HOST'),
-        port: env('SMTP_PORT'),
+        port: env.int('SMTP_PORT'),
         auth: {
           user: env('SMTP_USERNAME'),
           pass: env('SMTP_PASSWORD'),
         },
-        // ... any custom nodemailer options
+        // any custom options
       },
       settings: {
         defaultFrom: 'duniola4u@gmail.com',
@@ -38,5 +76,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  // ...
 });
