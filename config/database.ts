fa3f662c870c70e1efa6e1a37a7e748754e33d85
Sulaimon Ekundayo <topdandydot@gmail.com>
2025-06-16 10:25:12 +0300

    Update database ssl

--- config/database.ts
+++ config/database.ts
@@ -1,19 +1,20 @@
-    // strapi-api/config/database.js
-    export default ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT'),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), 
-          },
-          //ssl: env.bool('DATABASE_SSL', false), // Use for SSL connection
+// strapi-api/config/database.js
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ...(env.bool('DATABASE_SSL', false) && {
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
         },
-        debug: false,
-      },
-    });
+      }),
+    },
+    debug: false,
+  },
+});
